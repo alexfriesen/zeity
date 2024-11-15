a8db91b22610d74c0f9a51a40a5d76befffb0588
alexfriesen <friesen.alex.home@gmail.com>
2024-11-15 11:11:10 +0100

    feat(project): add project form

---
+++ apps/timer/app/components/ProjectForm.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import { z } from 'zod';
+import { nanoid } from 'nanoid';
+import type { FormSubmitEvent } from '@nuxt/ui';
+
+import { PROJECT_STATUS_ACTIVE, PROJECT_STATUS_CLOSED, PROJECT_STATUSES } from '~/types/project';
+
+const statusItems = [
+    { value: PROJECT_STATUS_ACTIVE, label: 'Active' },
+    { value: PROJECT_STATUS_CLOSED, label: 'Closed' },
+];
+
+const props = defineProps<{
+    data?: Schema;
+}>();
+const emits = defineEmits(['submit']);
+
+const schema = z.object({
+    id: z.string().default(nanoid()),
+    status: z.enum(PROJECT_STATUSES).default(PROJECT_STATUS_ACTIVE),
+    name: z.string().min(3).default(''),
+    notes: z.string().default(''),
+});
+type Schema = z.output<typeof schema>
+const state = ref<Partial<Schema>>({
+    id: undefined,
+    status: undefined,
+    name: '',
+    notes: '',
+});
+
+watch(() => props.data, (data) => {
+    if (data) {
+        state.value = data;
+    }
+}, { immediate: true });
+
+function handleSubmit(event: FormSubmitEvent<Schema>) {
+    emits('submit', event.data);
+}
+</script>
+
+<template>
+    <UForm :schema="schema" :state="state" class="space-y-4" @submit="handleSubmit">
+        <UFormField label="Name" name="name">
+            <UInput v-model="state.name" class="w-full" />
+        </UFormField>
+        <UFormField label="Status" name="status">
+            <USelect v-model="state.status" :items="statusItems" class="w-full" />
+        </UFormField>
+        <UFormField label="Notes" name="notes">
+            <UTextarea v-model="state.notes" class="w-full" autoresize />
+        </UFormField>
+
+        <UButton block type="submit">Save</UButton>
+    </UForm>
+</template>
---
+++ apps/timer/app/pages/projects/create.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { nanoid } from 'nanoid';
+
+import { PROJECT_STATUS_ACTIVE, type Project } from '~/types/project';
+
+const router = useRouter();
+const store = useProjectStore();
+
+const data = ref<Project>({
+    id: nanoid(),
+    status: PROJECT_STATUS_ACTIVE,
+    name: '',
+    notes: '',
+});
+
+function handleSubmit(data: Project) {
+    store.projects.insert(data);
+
+    router.push('/projects');
+}
+</script>
+
+<template>
+    <UContainer as="section" class="page my-3">
+        <h2>Create Project</h2>
+
+        <ProjectForm :data="data" @submit="handleSubmit" />
+    </UContainer>
+</template>

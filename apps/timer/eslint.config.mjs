af18544f5e5970ddf7340291e72da0e993b80a43
alexfriesen <friesen.alex.home@gmail.com>
2024-11-15 11:16:22 +0100

    ci: disable vue/first-attribute-linebreak lint rule

--- apps/timer/eslint.config.mjs
+++ apps/timer/eslint.config.mjs
@@ -3,4 +3,10 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/first-attribute-linebreak': 'off',
+    },
+  }
 )

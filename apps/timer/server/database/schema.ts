049a87784cac00541cef14fe51cb70947849ae32
alexfriesen <friesen.alex.home@gmail.com>
2024-12-25 21:35:10 +0100

    feat(database): add db schema

---
+++ apps/timer/server/database/schema.ts
@@ -0,0 +1,8 @@
+export { sessions } from './session';
+export { users } from './user';
+
+export { times } from './time';
+export { projects } from './project';
+
+export { organisations } from './organisation';
+export { organisationMembers } from './organisation-member';

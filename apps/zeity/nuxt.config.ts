240f2843d3f23213c981b086ac81a6340b3d6acb
Alexander Friesen <friesen.alex.home@gmail.com>
2025-04-03 17:53:33 +0200

    chore(all): update deps

--- apps/zeity/nuxt.config.ts
+++ apps/zeity/nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
   ],
   css: ['~/assets/css/main.css'],
   pwa: {
-    // disable: true,
+    disable: true,
     strategies: 'generateSW',
     registerType: 'autoUpdate',
     manifest: {

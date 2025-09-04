50e487d94317d99790eb89968a866a1b709ec2a4
Alexander Friesen <friesen.alex.home@gmail.com>
2025-09-04 12:55:54 +0200

    fix: add divider to main navigation

--- apps/zeity/app/layouts/default.vue
+++ apps/zeity/app/layouts/default.vue
@@ -3,7 +3,7 @@
         <AppHeader />
 
         <UContainer>
-            <div class="grid md:flex md:gap-4 min-h-[calc(100vh-var(--ui-header-height))]">
+            <div class="grid md:flex md:gap-4 min-h-[calc(100vh-var(--ui-header-height))] divide-x divide-default">
                 <aside
                     class="hidden md:block flex-shrink-0 sticky top-(--ui-header-height) max-h-[calc(100vh-var(--ui-header-height))] md:44 lg:w-52 xl:w-60 overflow-y-auto">
                     <AppAsideNavigation />
@@ -13,8 +13,8 @@
                     <slot />
                 </main>
             </div>
-
         </UContainer>
+
         <AppBottomNavigation class="md:hidden" />
     </div>
 </template>

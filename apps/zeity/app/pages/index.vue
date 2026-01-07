b59ff8ce26dc6b13b1f2afe19e336a08bd40f497
Alexander Friesen <friesen.alex.home@gmail.com>
2026-01-07 18:16:59 +0000

    feat: handle routing in app shell

--- apps/zeity/app/pages/index.vue
+++ apps/zeity/app/pages/index.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
-definePageMeta({
-    middleware: 'page-root',
+// definePageMeta({
+//     middleware: 'page-root',
+// })
+onMounted(() => {
+    const path = useRoute().path
+    if (path === '/') {
+        navigateTo('/home')
+    } else {
+        navigateTo(path)
+    }
 })
 </script>
 

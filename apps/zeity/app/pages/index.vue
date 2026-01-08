8f97d0ec5fce5723a1b448ab6723db5e2bc15380
Alexander Friesen <friesen.alex.home@gmail.com>
2026-01-08 09:48:44 +0000

    refactor: cleanup

--- apps/zeity/app/pages/index.vue
+++ apps/zeity/app/pages/index.vue
@@ -2,19 +2,6 @@
 definePageMeta({
     middleware: 'page-root',
 })
-// onMounted(() => {
-//     console.log('route', useRoute())
-//     const path = useRoute().path
-//     if (path === '/') {
-//         navigateTo('/timer')
-//     } else {
-//         navigateTo(path)
-//     }
-// })
-// useNuxtApp().hook('app:suspense:resolve', async () => {
-//     console.log('route', useRoute())
-// })
-
 </script>
 
 <template>
